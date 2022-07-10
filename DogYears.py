9189c8a6a7e4fde22ecc62c5e716d0f870664a6e
Pravin Raja <33183171+Pravinraja@users.noreply.github.com>
2022-07-10 18:02:45 -0400

    Add files via upload

---
+++ DogYears.py
@@ -0,0 +1,10 @@
+print('Enter your First name:')
+x = input()
+print('Enter your Last name:')
+y = input()
+print('What is your age?:')
+z=input()
+z2 = float(z)
+d=z2*7
+print('Hi, ' + x + ' ' + y + ' ' + 'you may be' + ' ' + z + ' ' + 'years old but in dog years you are' +' '+ str(d)+' '
++ 'years old')
